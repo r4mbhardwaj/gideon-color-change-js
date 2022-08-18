476bccb49ce5b4ca86c6ffa77d9bc508e96304cd
Ram <63110845+r4mbhardwaz@users.noreply.github.com>
2022-08-18 17:51:24 +0530

    Add files via upload

---
+++ color_dynamic_change.js
@@ -0,0 +1,14 @@
+// ::selection {background-color: #fd2626;}
+
+// ::-webkit-scrollbar-thumb { border: 1px solid #fd2626; }
+
+
+// a { color: #fbacac; }
+
+// body { color: #fd2626; }
+$('body').css('color','#fd2626');
+$('a').css('color','#fbacac');
+$('::-webkit-scrollbar-thumb').css('border','1px solid #fd2626');
+$('::selection').css('background-color','#fd2626');
+$('.frame-text').css('background-color','rgb(41 4 15 / 65%)');
+$('.c01238').css('border-color','#bb0202');
